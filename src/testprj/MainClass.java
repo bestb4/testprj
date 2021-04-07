8fc12596f4134253723a132bb0f377d62d518698
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-07 13:39:37 +0900

    14:객체와 메모리

--- src/testprj/MainClass.java
+++ src/testprj/MainClass.java
@@ -12,6 +12,13 @@ public class MainClass {
 			child1.getInfo("lemon","apple");
 		
 		
+			ObjectClass obj1 = new ObjectClass();
+			ObjectClass obj2 = new ObjectClass();
+			ObjectClass obj3 = new ObjectClass();
+			
+			System.out.println("obj1--->"+obj1);
+			System.out.println("obj2--->"+obj2);
+			System.out.println("obj3--->"+obj3);
 		}
 }
 	
