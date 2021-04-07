c4f852fd2fdd85466ce78a6acc5ab4c977ca0ada
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-07 21:54:06 +0900

    Gugudan

---
+++ FourFive.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class FourFive {
+    public static void main(String[] args) {
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("number : " + number*1);
+        System.out.println("number : " + number*2);
+        System.out.println("number : " + number*3);
+        System.out.println("number : " + number*4);
+        System.out.println("number : " + number*5);
+        System.out.println("number : " + number*6);
+        System.out.println("number : " + number*7);
+        System.out.println("number : " + number*8);
+        System.out.println("number : " + number*9);
+
+        // 구구단 출력하는 예제 구현        
+    }
+}
