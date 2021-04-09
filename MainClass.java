075ab60108bedae70125c53ae0c0cff6a4c3f7cc
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-09 16:28:23 +0900

    99dan_array

--- MainClass.java
+++ MainClass.java
@@ -1,10 +1,10 @@
 public class MainClass {
 	public static int[] calculate(int times) {
 		int[] result = new int[9];
-		
 		for(int i=0;i<result.length;i++) {
 			result[i]=times*(i+1);
-	} return result;
+	}
+		return result;
 	}
 	
 	public static void print(int[] result) {
@@ -14,18 +14,11 @@ public class MainClass {
 	}
 	
 	public static void main(String[] args) {
-	int[] result = calculate(2);
-	print(result);
-	
-	int[] times3 = calculate(3);
-	print(times3);
-	
-	int[] times4 = calculate(4);
-	print(times4);
-	
-	
-	}
-	
+		for (int i = 2;i<10;i++) {
+			int[] result = calculate(i);
+			print(result);
+		}
+	}	
 }
 
 	
@@ -153,12 +146,7 @@ public class MainClass {
 	
 	
 	
-	
-	
-	
-	
-		}
-	}
+
 
 	
 
