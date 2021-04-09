27fdda54df235a9462e9d386ada8c795af7d2a49
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-09 16:21:17 +0900

    99dan_array

--- MainClass.java
+++ MainClass.java
@@ -1,21 +1,34 @@
-import java.util.Scanner;
-
 public class MainClass {
-public static void main(String[] args) {
-	int[] result = new int[9];
-	for(int i=0;i<result.length;i++) {
-		result[i]=2*(i+1);
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		
+		for(int i=0;i<result.length;i++) {
+			result[i]=times*(i+1);
+	} return result;
 	}
-	for(int i=0;i<result.length;i++){
-		System.out.println(result[i]);
+	
+	public static void print(int[] result) {
+		for(int i=0;i<result.length;i++){
+			System.out.println(result[i]);
+		}
 	}
-	int[] times3 = new int[9];
-	for(int i=0;i<times3.length;i++) {
-		times3[i]=3*(i+1);
+	
+	public static void main(String[] args) {
+	int[] result = calculate(2);
+	print(result);
+	
+	int[] times3 = calculate(3);
+	print(times3);
+	
+	int[] times4 = calculate(4);
+	print(times4);
+	
+	
 	}
-	for(int i=0;i<times3.length;i++){
-		System.out.println(times3[i]);
-	}
+	
+}
+
+	
 	
 	
 	//2´Ü
