446213dcc590c72cb5cb3812626d12d07e315360
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-10 21:17:32 +0900

    variables

---
+++ NineNine.java
@@ -0,0 +1,29 @@
+
+public class NineNine {
+
+	public static void main(String[] args) {
+		System.out.println("2´Ü");
+		int result = 2*1;
+		System.out.println(result);
+		result = 2*2;
+		System.out.println(result);
+		result = 2*3;
+		System.out.println(result);
+		 result = 2*4;
+		System.out.println(result);
+		 result = 2*5;
+		System.out.println(result);
+		result = 2*6;
+		System.out.println(result);
+		 result = 2*7;
+		System.out.println(result);
+		 result = 2*8;
+		System.out.println(result);
+		 result = 2*9;
+		System.out.println(result);
+		
+	
+		
+		
+	}
+}
