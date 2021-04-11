0987465c86cca9060bdf3add189f787585f15f26
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-11 22:50:49 +0900

    Add files via upload

---
+++ TwoThree.java
@@ -0,0 +1,26 @@
+
+public class TwoThree {
+
+	public static void main(String[] args) {
+	int result = 2*1;
+		
+		System.out.println(result);
+	result = 2*2;
+	System.out.println(result);
+	result = 2*3;
+	System.out.println(result);
+	result = 2*4;
+	System.out.println(result);
+	result = 2*5;
+	System.out.println(result);
+	result = 2*6;
+	System.out.println(result);
+	result = 2*7;
+	System.out.println(result);
+	result = 2*8;
+			System.out.println(result);
+	result = 2*9;
+			System.out.println(result);
+	
+	}
+}
