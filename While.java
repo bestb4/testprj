32bd7c68e902c4c446bf74a69a235280c6cdf83a
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-12 19:55:12 +0900

    Add files via upload

---
+++ While.java
@@ -0,0 +1,19 @@
+public class While {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 10) {
+            // °ö¼À °á°ú Ãâ·Â
+        	System.out.println(1*i);
+        	System.out.println(2*i);
+        	System.out.println(3*i);
+        	System.out.println(4*i);
+        	System.out.println(5*i);
+        	System.out.println(6*i);
+        	System.out.println(7*i);
+        	System.out.println(8*i);
+        	System.out.println(9*i);
+            // i °ªÀ» 1 Áõ°¡
+        	i++;
+        }
+    }
+}
